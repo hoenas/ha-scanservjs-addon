b197505772bad6d6038b0dc32caed67e46ca6bc9
vismjs <jonas.maas@vector.com>
2025-04-16 18:49:21 +0200

    Fixed typo

--- scanservjs/config.local.js
+++ scanservjs/config.local.js
@@ -125,7 +125,7 @@ module.exports = {
     // Copy the file to the specified directory
     copy_destination = process.env.COPY_SCANS_TO
     // TODO: Fix location
-    return await Process.spawn(`'cp ${fileInfo.fullname}' '/share/paperless/consume/'`);
+    return await Process.spawn(`cp '${fileInfo.fullname}' '/share/paperless/consume/'`);
   },
 
   /**
